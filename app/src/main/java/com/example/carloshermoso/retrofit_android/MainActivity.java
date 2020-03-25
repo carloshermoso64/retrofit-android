edbad9cc8b213cb8efda8a0cade4e865a150972b
carloshermoso64 <59064594+carloshermoso64@users.noreply.github.com>
2020-03-25 20:28:38 +0100

    Retrofit working

--- app/src/main/java/com/example/carloshermoso/retrofit_android/MainActivity.java
+++ app/src/main/java/com/example/carloshermoso/retrofit_android/MainActivity.java
@@ -1,8 +1,20 @@
 package com.example.carloshermoso.retrofit_android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +22,60 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button bt = (Button) findViewById(R.id.button);
+        final TextView tv = (TextView) findViewById(R.id.textView);
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+                interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+                OkHttpClient client = new OkHttpClient.Builder()
+                        .addInterceptor(interceptor)
+                        .build();
+
+
+                Retrofit retrofit = new Retrofit.Builder()
+                        .baseUrl("https://api.github.com/")
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .client(client)
+                        .build();
+
+                GitHubService service = retrofit.create(GitHubService.class);
+
+                Call<List<Repo>> repos = service.listRepos("carloshermoso64");
+
+
+                repos.enqueue(new Callback<List<Repo>>() {
+                    @Override
+                    public void onResponse(Call<List<Repo>> call, Response<List<Repo>> response) {
+
+                        List<Repo> repos = response.body();
+                        String texto ="[";
+                        for (Repo r : repos){
+                            texto = texto + r.full_name+ " ";
+
+                        }
+                        texto= texto + "]";
+
+
+
+
+                        tv.setText(texto);
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<Repo>> call, Throwable t) {
+                        tv.setText("Fallo");
+                    }
+                });
+            }
+        });
+
+
     }
 }
